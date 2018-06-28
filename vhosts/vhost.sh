0faf54ff8afa7895eca595e1e7e300c6a2e5c3c9
Lukasz Wisniewski <lwisniewski387@gmail.com>
2018-06-28 20:43:31 +0100

    Proxy for redis-commander

--- vhosts/vhost.sh
+++ vhosts/vhost.sh
@@ -87,7 +87,7 @@ cat >> /etc/nginx/conf.d/default.conf <<- EOF
     location /toolbox/redis-commander {
         proxy_pass http://redis-commander:8081;
     }
-    
+
     location ~ \.php$ {
         try_files \$uri =404;
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
