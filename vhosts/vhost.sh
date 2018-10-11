739a5205d1191b6e0e1c968cfcef973f68692c63
Lukasz Wisniewski <lwisniewski387@gmail.com>
2018-10-11 01:10:14 +0100

    Configuration fix for wordpress vhost

--- vhosts/vhost.sh
+++ vhosts/vhost.sh
@@ -106,6 +106,11 @@ fi
 if [ "$APPLICATION" = "wordpress" ]; then
 if [ "$NGINX_SSL" = true ]; then
 cat > /etc/nginx/conf.d/default.conf <<- EOF
+upstream php {
+        server unix:/tmp/php-cgi.socket;
+        server php:9000;
+}
+
 server {
     listen 80;
     server_name $DEV_DOMAIN;
@@ -130,17 +135,14 @@ server {
     }
 
     location ~ \.php$ {
-        try_files \$uri =404;
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        include fastcgi.conf;
+        fastcgi_intercept_errors on;
         fastcgi_pass php:9000;
-        fastcgi_index index.php;
-        include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        fastcgi_param PATH_INFO \$fastcgi_path_info;
     }
 
-    location ~* \.(jpg|jpeg|png|gif|ico|css|js)$ {
-            expires 365d;
+    location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
+            expires max;
+            log_not_found off;
     }
 
     client_max_body_size 100M;
@@ -148,6 +150,11 @@ server {
 EOF
 else
 cat > /etc/nginx/conf.d/default.conf <<- EOF
+upstream php {
+        server unix:/tmp/php-cgi.socket;
+        server php:9000;
+}
+
 server {
     index index.php index.html;
     server_name $DEV_DOMAIN;
@@ -156,21 +163,18 @@ server {
     root /code;
 
     location / {
-            try_files \$uri \$uri/ /index.php?\$args;
+        try_files \$uri \$uri/ /index.php?\$args;
     }
 
     location ~ \.php$ {
-        try_files \$uri =404;
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        include fastcgi.conf;
+        fastcgi_intercept_errors on;
         fastcgi_pass php:9000;
-        fastcgi_index index.php;
-        include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        fastcgi_param PATH_INFO \$fastcgi_path_info;
     }
 
-    location ~* \.(jpg|jpeg|png|gif|ico|css|js)$ {
-        expires 365d;
+    location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
+            expires max;
+            log_not_found off;
     }
 
     client_max_body_size 100M;
